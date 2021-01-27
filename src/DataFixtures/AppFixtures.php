caf2dff06a95b68cdabdaae3b59630d1e32bfcd1
chloeadr <chloeadrian98@gmail.com>
2021-01-27 15:22:34 +0100

    Exam start

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,25 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use Faker\Factory;
+use App\Entity\Product;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = \Faker\Factory::create('en-GB');
+
+        for ($i = 0; $i < 50; $i++) {
+        $product = new Product();
+        $product->setName("Product " . $i)
+                ->setDescription($faker->realText(400))
+                ->setPrice(rand(100,6000));
+
+        $manager->persist($product);
+        }
 
         $manager->flush();
     }
